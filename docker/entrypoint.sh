379bca90c0de0a6da98b7ffd84d52d00b9771493
y <y@g.y>
2025-12-23 15:22:17 +0800

    1

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -286,10 +286,12 @@ if [[ "${ENABLE_TASKEXECUTOR}" -eq 1 ]]; then
     fi
 fi
 
-wait
-
 
 # 安装本地保存的 PyMuPDF 库
 echo "Installing PyMuPDF from local files..."
 pip install --no-index --find-links=/ragflow/libs PyMuPDF
+
+
+
+wait
 
